2a609ef83ced3ed3ac857deeb4c8a1a548db9b04
andy <andrew.bandura@gmail.com>
2018-02-06 23:29:12 +0200

    - added javadoc annotations for services

--- src/main/java/service/AirlineCompanyService.java
+++ src/main/java/service/AirlineCompanyService.java
@@ -8,14 +8,33 @@ import java.util.List;
  */
 
 
-public interface AirlineCompanyService {
+public interface AirlineCompanyService{
 
-    int calculateTotalCapacity();
+        /**
+         * Returns a calculated total capacity of all aircraft.
+         * @return an int value.
+         */
+        int calculateTotalCapacity();
 
-    int calculateCarryingCapacity();
+        /**
+         * Returns a calculated carrying capacity of all aircraft.
+         * @return an int value.
+         */
+        int calculateCarryingCapacity();
 
-    List<Aircraft> getSortedAircraftList();
+        /**
+         * Returns an aircraft list sorted by flight range.
+         * @return a list of Aircraft.
+         */
+        List<Aircraft> getSortedAircraftList();
 
-    List<Aircraft> findByFuelConsumptionRange(double from, double to);
+        /**
+         * Returns an aircraft list filtered by fuel consumption range.
+         *
+         * @param from the double value, min in the range.
+         * @param to   the double value, max in the range.
+         * @return a list of Aircraft.
+         */
+        List<Aircraft> findByFuelConsumptionRange(double from, double to);
 
 }
