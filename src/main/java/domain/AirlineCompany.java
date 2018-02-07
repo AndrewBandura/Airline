798953c0ab714afeb8bcb2c9a189eb508a6d7357
andy <andrew.bandura@gmail.com>
2018-02-07 02:32:37 +0200

    - added GSon dependency - added util for loading aircraft from json file - added new test - made some refactoring

--- src/main/java/domain/AirlineCompany.java
+++ src/main/java/domain/AirlineCompany.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import util.JsonUtil;
 
 import java.util.Arrays;
 import java.util.List;
@@ -21,11 +22,8 @@ public class AirlineCompany {
     private List<Aircraft> aircrafts;
 
     public void init(){
-        Aircraft turboPlane = new TurboPropPlane("AN-22",1000, 55, 50, 300.7, 2);
-        Aircraft jetPlane1 = new JetPlane("Boeing-737",4000, 155, 150, 1000, 2);
-        Aircraft jetPlane2 = new JetPlane("Airbus-180",2000, 100, 95, 700.2, 1);
 
-        this.aircrafts = Arrays.asList(turboPlane, jetPlane1, jetPlane2);
+        this.aircrafts = JsonUtil.parse();
 
     }
 
